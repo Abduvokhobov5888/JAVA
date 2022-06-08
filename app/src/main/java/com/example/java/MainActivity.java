407c501764792ed213a5049dc0b34c1689310500
Abbosbek <vip.real.aa@gmail.com>
2022-06-08 12:03:45 +0500

    2 chi Serializable

--- app/src/main/java/com/example/java/MainActivity.java
+++ app/src/main/java/com/example/java/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.java;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.java.model.Users;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = MainActivity.class.toString();
+   // int LAUNCH_DETAIL = 1001;
+    TextView tv_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +33,33 @@ public class MainActivity extends AppCompatActivity {
         initViews();
     }
 
+    ActivityResultLauncher<Intent> detailLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        String text = data.getStringExtra("result");
+                        Log.d(TAG, text);
+                        tv_home.setText(text);
+                    }
+                }
+            }
+    );
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Activity.RESULT_OK){
+            String result = data.getStringExtra("result");
+            Log.d(TAG,result);
+            tv_home.setText(result);
+        }
+    }
+
     void initViews(){
+        tv_home = findViewById(R.id.tv_home);
         Button button_open = (Button) findViewById(R.id.button_open);
         button_open.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
     void openSecond(Users user){
         Intent intent = new Intent(this, SecondActivity.class);
         intent.putExtra("user", user);
-        startActivity(intent);
+        detailLauncher.launch(intent);
+        //startActivity(intent);
     }
 }
--- app/src/main/java/com/example/java/SecondActivity.java
+++ app/src/main/java/com/example/java/SecondActivity.java
@@ -1,9 +1,14 @@
 package com.example.java;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.java.model.Users;
@@ -12,15 +17,28 @@ public class SecondActivity extends AppCompatActivity {
     static final String TAG = SecondActivity.class.toString();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         initViews();
     }
     void initViews(){
         TextView text_id = findViewById(R.id.text_id);
+        Button b_exit = findViewById(R.id.b_exit);
+        b_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToFinish();
+            }
+        });
         Users user =(Users) getIntent().getSerializableExtra("user");
         Log.d(TAG,user.toString());
         text_id.setText(user.toString());
+    }
+    void backToFinish(){
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("result","Hammaga rahmat!");
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
     }
 }
