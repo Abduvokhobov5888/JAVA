fd0085455f62af4ecb20de7ae172ba9f1197b0a3
Abbosbek <vip.real.aa@gmail.com>
2022-06-24 11:46:38 +0500

    TableLayout

---
+++ app/src/main/java/com/example/java/Activity/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.java.Activity;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.java.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
