a1e1257d0ba385c95d5a9ff6ed3730872c5de1f6
Abbosbek <vip.real.aa@gmail.com>
2022-06-28 11:20:31 +0500

    TextInputLayout

--- app/src/main/java/com/example/java/Activity/MainActivity.java
+++ app/src/main/java/com/example/java/Activity/MainActivity.java
@@ -8,7 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -20,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initViews();
     }
-
+//    void initViews(){
+//        TextView textView = findViewById(R.id.textView);
+//        String text = "I know just to #whister, And I know how to dry, I know just where to find the anser";
+//
+//        Spannable spannable = new SpannableString(text);
+//        spannable.setSpan(new ForegroundColorSpan(Color.BLUE),18,27, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//
+//        textView.setText(spannable);
+//    }
 }
