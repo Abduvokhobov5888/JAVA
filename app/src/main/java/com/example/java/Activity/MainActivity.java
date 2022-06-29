6c7f675078386932ab799a6f4877788188685f57
Abbosbek <vip.real.aa@gmail.com>
2022-06-29 19:44:54 +0500

    GridView

--- app/src/main/java/com/example/java/Activity/MainActivity.java
+++ app/src/main/java/com/example/java/Activity/MainActivity.java
@@ -15,25 +15,39 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.java.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView gridView;
+
+    String[] numberWord = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
+
+    int[] numberImage = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five,
+            R.drawable.six, R.drawable.seven, R.drawable.eight, R.drawable.nine, R.drawable.ten};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initViews();
+
+        gridView = findViewById(R.id.grid_view);
+
+        MainAdapter adapter = new MainAdapter(MainActivity.this,numberWord,numberImage);
+        gridView.setAdapter(adapter);
+//
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(getApplicationContext(), "You Clicked" + numberWord[+
+//                        position], Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
-//    void initViews(){
-//        TextView textView = findViewById(R.id.textView);
-//        String text = "I know just to #whister, And I know how to dry, I know just where to find the anser";
-//
-//        Spannable spannable = new SpannableString(text);
-//        spannable.setSpan(new ForegroundColorSpan(Color.BLUE),18,27, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//
-//        textView.setText(spannable);
-//    }
 }
