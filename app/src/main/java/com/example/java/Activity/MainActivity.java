71afe9e90f46754df9b9940f7aee497612e35679
Abbosbek <vip.real.aa@gmail.com>
2022-07-04 11:49:36 +0500

    RecyclerView with NestedScrollView

--- app/src/main/java/com/example/java/Activity/MainActivity.java
+++ app/src/main/java/com/example/java/Activity/MainActivity.java
@@ -1,53 +1,64 @@
 package com.example.java.Activity;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.java.Adapter.Adapter;
 import com.example.java.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    GridView gridView;
+    RecyclerView dataList;
+    NestedScrollView nestedScrollview;
+    List<String> titles;
+    List<Integer> images;
 
-    String[] numberWord = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
-
-    int[] numberImage = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five,
-            R.drawable.six, R.drawable.seven, R.drawable.eight, R.drawable.nine, R.drawable.ten};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gridView = findViewById(R.id.grid_view);
+        dataList = findViewById(R.id.recyclerView);
+        nestedScrollview = findViewById(R.id.nestedScrollview);
 
-        MainAdapter adapter = new MainAdapter(MainActivity.this,numberWord,numberImage);
-        gridView.setAdapter(adapter);
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
 
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "You Clicked " + numberWord[+
-                        position], Toast.LENGTH_SHORT).show();
-            }
-        });
+        titles.add("One");
+        titles.add("Two");
+        titles.add("Three");
+        titles.add("Four");
+        titles.add("Five");
+        titles.add("Six");
+        titles.add("Seven");
+        titles.add("Eight");
+        titles.add("Nine");
+        titles.add("Ten");
+
+        images.add(R.drawable.one);
+        images.add(R.drawable.two);
+        images.add(R.drawable.three);
+        images.add(R.drawable.four);
+        images.add(R.drawable.five);
+        images.add(R.drawable.six);
+        images.add(R.drawable.seven);
+        images.add(R.drawable.eight);
+        images.add(R.drawable.nine);
+        images.add(R.drawable.ten);
+
+
+        Adapter adapter = new Adapter(this, titles, images);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        dataList.setNestedScrollingEnabled(false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
     }
 }
